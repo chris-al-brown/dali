ab3fce52f91c56306d6a2c07f7ae5e6bf142531c
Chris Brown <chris.al.brown@gmail.com>
2017-02-23 14:47:35 +0100

    Added AST.Expression and AST.Primary visitor patterns: - moved printing and coloring to a separate console with env detection

--- Sources/Dali.swift
+++ Sources/Dali.swift
@@ -28,90 +28,35 @@
 import Foundation
 
 public final class Dali {
-
-    public enum ANSIColor: String {
-        case black      = "\u{001B}[0;30m"
-        case red        = "\u{001B}[0;31m"
-        case green      = "\u{001B}[0;32m"
-        case yellow     = "\u{001B}[0;33m"
-        case blue       = "\u{001B}[0;34m"
-        case magenta    = "\u{001B}[0;35m"
-        case cyan       = "\u{001B}[0;36m"
-        case white      = "\u{001B}[0;37m"
-
-        public func apply(_ item: Any) -> String {
-            return "\(rawValue)\(item)\u{001B}[0;0m"
-        }
-    }
-
-    /// Checks whether Xcode spawned the process (i.e. ANSI color codes not supported)
-    public enum Environment: CustomStringConvertible {
-        case terminal
-        case xcode
-        
-        public init() {
-            let environment = ProcessInfo.processInfo.environment
-            let xcode = environment["XPC_SERVICE_NAME"]?.range(of:"Xcode") != nil
-            self = xcode ? .xcode : .terminal
-        }
-        
-        public var description: String {
-            switch self {
-            case .terminal:
-                return "iterm"
-            case .xcode:
-                return "xcode"
-            }
-        }
+    
+    public enum Mode {
+        case file(String)
+        case repl
+        case test
+        case usage
     }
     
-    public enum Input: CustomStringConvertible {
-        case debug
-        case file(String)
-        case stdin
-        case usage
-        
-        public var description: String {
-            switch self {
-            case .debug:
-                return "<\(DevURandom())>"
-            case .file(let value):
-                return "file: '\(value)'"
-            case .stdin:
-                return "<stdin>"
-            case .usage:
-                var output = ""
-                output += "Usage:" + "\n"
-                output += "  " + "dali" + "\n"
-                output += "  " + "dali <script>" + "\n"
-                output += "  " + "dali --debug" + "\n"
-                output += "  " + "dali --help" + "\n"
-                return output
-            }
-        }
-    }
-
     public enum Status {
         case success
         case failure
     }
-    
-    public init(_ args: [String], env: Environment = Environment()) {
-        self.env = env
-        if args.count == 1 {
-            self.input = .stdin
-        } else if args.count == 2 {
+
+    public init(_ args: [String]) {
+        self.console = Console()
+        switch args.count {
+        case 1:
+            self.mode = .repl
+        case 2:
             switch args[1] {
-            case "--debug":
-                self.input = .debug
-            case "--help":
-                self.input = .usage
+            case "-t", "--test":
+                self.mode = .test
+            case "-h", "--help":
+                self.mode = .usage
             default:
-                self.input = .file(args[1])
+                self.mode = .file(args[1])
             }
-        } else {
-            Swift.print(args)
-            self.input = .usage
+        default:
+            self.mode = .usage
         }
     }
     
@@ -122,196 +67,28 @@ public final class Dali {
             do {
                 let parser = Parser(tokens)
                 let expressions = try parser.parse()
-                for expression in expressions {
-                    print(format(expression))
-                }
+                expressions.forEach { console.log($0) }
                 return .success
             } catch let issue as Parser.Error {
-                error(issue, in:source)
+                console.error(issue, in:source)
                 return .failure
             } catch let other {
                 fatalError("Expected a ParserError: \(other)")
             }
         } catch let issue as Scanner.Error {
-            error(issue, in:source)
+            console.error(issue, in:source)
             return .failure
         } catch let other {
             fatalError("Expected a ScannerError: \(other)")
         }
     }
     
-    private func error(_ message: String) {
-        print(message, color:.red)
-    }
-    
-    private func error(_ issue: Parser.Error, in source: Source) {
-        print(format(issue, in:source))
-    }
-    
-    private func error(_ issue: Scanner.Error, in source: Source) {
-        print(format(issue, in:source))
-    }
-
     public func exit(with status: Status) -> Never {
         switch status {
         case .success:
             Darwin.exit(EXIT_SUCCESS)
         case .failure:
             Darwin.exit(EXIT_FAILURE)
-        }
-    }
-
-    private func format(_ value: AST.Expression) -> String {
-        switch value {
-        case .assign(let key, let value):
-            var output = ""
-            output += useColor ? ANSIColor.cyan.apply(key) : key
-            output += ":"
-            output += " "
-            output += format(value)
-            return output
-        case .binary(let lhs, let op, let rhs):
-            var output = ""
-            output += "("
-            output += format(lhs)
-            output += " "
-            output += useColor ? ANSIColor.green.apply(op.lexeme.description) : op.lexeme.description
-            output += " "
-            output += format(rhs)
-            output += ")"
-            return output
-        case .call(let lhs, let args):
-            var output = ""
-            output += format(lhs)
-            output += "("
-            output += args.reduce("") {
-                let key = useColor ? ANSIColor.cyan.apply($0.1.0) : $0.1.0
-                let value = format($0.1.1)
-                return $0.0 + key + ": " + value + ", "
-            }
-            if !args.isEmpty {
-                let _ = output.unicodeScalars.popLast()
-                let _ = output.unicodeScalars.popLast()
-            }
-            output += ")"
-            return output
-        case .get(let lhs, let index):
-            var output = ""
-            output += format(lhs)
-            output += "["
-            output += format(index)
-            output += "]"
-            return output
-        case .primary(let primary):
-            return format(primary)
-        case .set(let lhs, let index, let rhs):
-            var output = ""
-            output += format(lhs)
-            output += "["
-            output += format(index)
-            output += "]"
-            output += ":"
-            output += " "
-            output += format(rhs)
-            return output
-        case .unary(let op, let rhs):
-            var output = ""
-            output += "("
-            output += useColor ? ANSIColor.green.apply(op.lexeme.description) : op.lexeme.description
-            output += format(rhs)
-            output += ")"
-            return output
-        }
-    }
-    
-    private func format(_ value: AST.Primary) -> String {
-        switch value {
-        case .boolean(let value):
-            return useColor ? ANSIColor.yellow.apply(value.description) : value.description
-        case .function(let args, let body):
-            var output = ""
-            output += useColor ? ANSIColor.green.apply("@") : "@"
-            output += "("
-            output += args.reduce("") {
-                let arg = useColor ? ANSIColor.cyan.apply($0.1) : $0.1
-                return $0.0 + arg + ", "
-            }
-            if !args.isEmpty {
-                let _ = output.unicodeScalars.popLast()
-                let _ = output.unicodeScalars.popLast()
-            }
-            output += ") {"
-            output += body.reduce("") {
-                return $0.0 + format($0.1) + ", "
-            }
-            if !body.isEmpty {
-                let _ = output.unicodeScalars.popLast()
-                let _ = output.unicodeScalars.popLast()
-            }
-            output += "}"
-            return output
-        case .identifier(let value):
-            return useColor ? ANSIColor.cyan.apply(value) : value
-        case .keyword(let value):
-            return useColor ? ANSIColor.yellow.apply(value.rawValue) : value.rawValue
-        case .list(let values):
-            var output = ""
-            output += "["
-            output += values.reduce("") {
-                return $0.0 + format($0.1) + ", "
-            }
-            if !values.isEmpty {
-                let _ = output.unicodeScalars.popLast()
-                let _ = output.unicodeScalars.popLast()
-            }
-            output += "]"
-            return output
-        case .map(let values):
-            var output = ""
-            output += "{"
-            output += values.reduce("") {
-                let key = useColor ? ANSIColor.cyan.apply($0.1.0) : $0.1.0
-                let value = format($0.1.1)
-                return $0.0 + key + ": " + value + ", "
-            }
-            if !values.isEmpty {
-                let _ = output.unicodeScalars.popLast()
-                let _ = output.unicodeScalars.popLast()
-            }
-            output += "}"
-            return output
-        case .number(let value):
-            return useColor ? ANSIColor.blue.apply(value.description) : value.description
-        case .string(let value):
-            return useColor ? ANSIColor.magenta.apply("\"" + value + "\"") : "\"" + value + "\""
-        }
-    }
-    
-    private func format(_ issue: Parser.Error, in source: Source) -> String {
-        return format(issue.description, in:source, at:issue.location)
-    }
-
-    private func format(_ issue: Scanner.Error, in source: Source) -> String {
-        return format(issue.description, in:source, at:issue.location)
-    }
-    
-    private func format(_ message: String, in source: Source, at location: Source.Location) -> String {
-        let row = source.line(for:location)
-        let col = source.columns(for:location)
-        var output = ""
-        output += "file: \(input), line: \(row), "
-        output += (col.count == 1) ? "column: \(col.lowerBound)\n" : "columns: \(col.lowerBound)-\(col.upperBound)\n"
-        output += source.extractLine(location) + "\n"
-        output += String(repeating:" ", count:col.lowerBound - 1) + String(repeating:"^", count:col.count) + "\n"
-        output += "SyntaxError: \(message)"
-        return useColor ? ANSIColor.red.apply(output) : output
-    }
-    
-    private func print(_ message: String, color: ANSIColor? = nil, terminator: String = "\n") {
-        if let kuler = color, useColor {
-            Swift.print(kuler.apply(message), separator:"", terminator:terminator)
-        } else {
-            Swift.print(message, separator:"", terminator:terminator)
         }
     }
     
@@ -370,43 +147,29 @@ public final class Dali {
     }
     
     public func run() -> Never {
-        switch input {
-        case .debug:
-            var xoroshiro = Xoroshiro128Plus()
-            var result: Status = .failure
-            while result == .failure {
-                let expression: AST.Expression = random(&xoroshiro, complexity:2)
-                let current = env
-                env = .xcode
-                let source = Source(format(expression))
-                env = current
-                result = compile(source)
-            }
-            exit(with:result)
+        switch mode {
         case .file(let name):
             do {
-                let source = Source(try String(contentsOfFile:name, encoding:.utf8))
+                let source = Source(try String(contentsOfFile:name, encoding:.utf8), input:"'\(name)'")
                 let result = compile(source)
                 exit(with:result)
             } catch let issue {
-                error("ScriptError: \(issue.localizedDescription)")
+                console.error("ScriptError: \(issue.localizedDescription)")
                 exit(with:.failure)
             }
-        case .stdin:
-            print("-------------------------------------")
-            print(" dali REPL v0.1.0 (press ^C to exit) ")
-            print("-------------------------------------")
-            let prompt  = ">>>"
-            let tab     = "   "
+        case .repl:
+            console.log("-------------------------------------")
+            console.log(" dali REPL v0.1.0 (press ^C to exit) ")
+            console.log("-------------------------------------")
             var buffer = ""
             while true {
-                print(prompt, terminator:" ")
+                console.prompt()
                 while true {
                     guard let line = readLine(strippingNewline:false) else { break }
                     buffer += line
-                    let source = Source(buffer)
+                    let source = Source(buffer, input:"<stdin>")
                     if source.needsContinuation {
-                        print(tab, terminator:" ")
+                        console.prompt(isContinuation:true)
                     } else {
                         let _ = compile(source)
                         buffer.removeAll(keepingCapacity:true)
@@ -414,16 +177,25 @@ public final class Dali {
                     }
                 }
             }
+        case .test:
+            var xoroshiro = Xoroshiro128Plus()
+            let formatter = Console.Formatter(useColor:false)
+            let expression: AST.Expression = random(&xoroshiro, complexity:2)
+            let source = Source(expression.accept(formatter), input:"</dev/urandom>")
+            let result = compile(source)
+            exit(with:result)
         case .usage:
-            print("\(input)")
+            var output = ""
+            output += "Usage:\n"
+            output += "    dali \n"
+            output += "    dali <script> \n"
+            output += "    dali (-t | --test) \n"
+            output += "    dali (-h | --help)"
+            console.log(output)
             exit(with:.success)
         }
     }
-
-    private var useColor: Bool {
-        return env == .terminal
-    }
     
-    private var env: Environment
-    private let input: Input
+    private let console: Console
+    private let mode: Mode
 }
